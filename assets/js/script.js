5775ddf087a677474c55f639cf79dacc8a63e926
Kriti Kumari <1905243@kiit.ac.in>
2021-02-01 01:12:21 +0530

    added resource page

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,7 @@
     $(document).ready(function(){
       $(window).scroll(function() {
         if ($(document).scrollTop() > 100) { 
-          $(".navbar").css("background-color", "skyblue"); 
+          $(".navbar").css("background-color", "white"); 
         } else {
           $(".navbar").css("background-color", "transparent"); 
         }
