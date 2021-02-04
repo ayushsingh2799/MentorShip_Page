2d510965534b99fa0878c056a0810062fd4e5521
Kriti Kumari <1905243@kiit.ac.in>
2021-02-04 14:04:46 +0530

    Updated nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,42 +60,8 @@
         }
       }
     });
-  
-    // Mobile Navigation
-    if ($('.nav-menu').length) {
-      var $mobile_nav = $('.nav-menu').clone().prop({
-        class: 'mobile-nav d-lg-none'
-      });
-      $('body').append($mobile_nav);
-      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-      $('body').append('<div class="mobile-nav-overly"></div>');
-  
-      $(document).on('click', '.mobile-nav-toggle', function(e) {
-        $('body').toggleClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-        $('.mobile-nav-overly').toggle();
-      });
-  
-      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-        e.preventDefault();
-        $(this).next().slideToggle(300);
-        $(this).parent().toggleClass('active');
-      });
-  
-      $(document).click(function(e) {
-        var container = $(".mobile-nav, .mobile-nav-toggle");
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-            $('.mobile-nav-overly').fadeOut();
-          }
-        }
-      });
-    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-      $(".mobile-nav, .mobile-nav-toggle").hide();
-    }
-  
+   
+   
     // Navigation active state on scroll
     $(document).ready(function(){
       $(window).scroll(function() {
@@ -200,8 +166,8 @@
     player.stopVideo();
   }
 
-
-
+  // ==============================
+  // For show btn==================
   $('#go').on('click',function(){
     $('#two').show('slow');
 });
